fedb3739beea5d496f2b8991d9c52db399239d75
Krzysztof Maciejczuk <krzysztof@maciejczuk.pl>
2022-05-23 13:27:15 +0200

    Changed user and category display

--- home_finances/account/serializers.py
+++ home_finances/account/serializers.py
@@ -1,9 +1,25 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from account.models import Operation
+from account.models import Operation, OperationCategory
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OperationCategory
+        fields = ("name", )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name')
 
 
 class OperationSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    user = UserSerializer()
+
     class Meta:
         model = Operation
         fields = ('title', 'date', 'amount', 'category', 'user')
