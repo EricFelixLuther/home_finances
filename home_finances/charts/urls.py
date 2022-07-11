111320d771789bebf5d5c5a22698ce88fdcdb15a
Krzysztof Maciejczuk <krzysztof@maciejczuk.pl>
2022-07-11 20:52:53 +0200

    Two pie charts - monthly expenses and category expenses yearly summary

--- home_finances/charts/urls.py
+++ home_finances/charts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from charts.api import LineChartApiView, ExpensesBarChartApiView
+from charts.api import LineChartApiView, ExpensesBarChartApiView, ExpensesPieChartYearSummary, \
+    ExpensesCategoriesPieChartYearSummary
 
 urlpatterns = [
     path('line', LineChartApiView.as_view(), name="line_chart"),
     path('expenses', ExpensesBarChartApiView.as_view(), name="expenses_chart"),
+    path('pie', ExpensesPieChartYearSummary.as_view(), name="expenses_pie"),
+    path('categories_pie', ExpensesCategoriesPieChartYearSummary.as_view(), name="categories_pie"),
 ]
