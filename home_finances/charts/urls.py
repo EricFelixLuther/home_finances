a74083c5ac74acc5091c879b47972cf4cbd7e35d
Krzysztof Maciejczuk <krzysztof@maciejczuk.pl>
2022-07-21 12:56:26 +0200

    Changed alerts to bootstrap alerts

--- home_finances/charts/urls.py
+++ home_finances/charts/urls.py
@@ -5,7 +5,7 @@ from charts.api import LineChartApiView, ExpensesBarChartApiView, ExpensesPieCha
 
 urlpatterns = [
     path('line', LineChartApiView.as_view(), name="line_chart"),
-    path('expenses', ExpensesBarChartApiView.as_view(), name="expenses_chart"),
+    path('expenses', ExpensesBarChartApiView.as_view(), name="expenses_bar"),
     path('pie', ExpensesPieChartYearSummary.as_view(), name="expenses_pie"),
     path('categories_pie', ExpensesCategoriesPieChartYearSummary.as_view(), name="categories_pie"),
 ]
