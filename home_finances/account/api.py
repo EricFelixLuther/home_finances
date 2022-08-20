d178c920ce0f06e7e8d609fdfd1591445ab02a57
Krzysztof Maciejczuk <krzysztof@maciejczuk.pl>
2022-08-20 09:35:44 +0200

    Filtering backend works

--- home_finances/account/api.py
+++ home_finances/account/api.py
@@ -1,7 +1,7 @@
-from datetime import date
+from datetime import date, datetime
 
 import django_filters
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from account.models import Operation
 from account.serializers import OperationSerializer
 from account.year_mixin import YearMixin
@@ -11,7 +11,9 @@ class OperationsViewSet(YearMixin, viewsets.ModelViewSet):
     serializer_class = OperationSerializer
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = None
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filter_backends = [filters.SearchFilter, django_filters.rest_framework.DjangoFilterBackend]
+    filterset_fields = ['title', 'category', 'user']
+    search_fields = ['title', 'user__first_name', 'category__name']
 
     def get_queryset(self):
         year = self._get_year(self.request)
@@ -28,5 +30,14 @@ class OperationsViewSet(YearMixin, viewsets.ModelViewSet):
                 day=31
             )
         )
-        queryset = self.filter_queryset(queryset).order_by("-date", "id")
+        queryset = self.filter_queryset(queryset)
+        queryset = self.filter_date_from_to(queryset)
+        queryset = queryset.order_by("-date", "-id")
+        return queryset
+
+    def filter_date_from_to(self, queryset):
+        if date_from := self.request.GET.get('date_from'):
+            queryset = queryset.filter(date__gte=datetime.strptime(date_from, "%Y-%m-%d"))
+        if date_to := self.request.GET.get('date_to'):
+            queryset = queryset.filter(date__lte=datetime.strptime(date_to, "%Y-%m-%d"))
         return queryset
